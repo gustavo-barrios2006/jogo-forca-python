6cdea88afd88d97b7944d74409d06667b197a37f
gustavo-barrios2006 <gabarrios2006@gmail.com>
2024-07-02 16:17:54 -0300

    modificando o código para que não haja distinsão entre letras maiúsculas e minúsculas e que não leve em conta possíveis espaços em branco no início e no fim da entrada.

--- forca.py
+++ forca.py
@@ -6,10 +6,10 @@ def jogar():
     acertou=False
     enforcou=False
     while(not acertou and not enforcou):
-        chute=input("Digite uma letra")
+        chute=input("Digite uma letra").strip()
         indice=0
         for letra in palavra_secreta:
-            if(letra==chute):
+            if(letra.upper()==chute.upper()):
                 print("Encontrei a letra {} na posição {}".format(letra, indice+1))
             indice+=1
     print("Fim do jogo")
