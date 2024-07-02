5676b38bf28c1a67da3810bb3d3697d5a2c4ddda
gustavo-barrios2006 <gabarrios2006@gmail.com>
2024-07-02 16:09:10 -0300

    adicionando a lógica de buscar a letra digitada na palavra secreta

--- forca.py
+++ forca.py
@@ -2,7 +2,16 @@ def jogar():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
-
+    palavra_secreta="banana"
+    acertou=False
+    enforcou=False
+    while(not acertou and not enforcou):
+        chute=input("Digite uma letra")
+        indice=0
+        for letra in palavra_secreta:
+            if(letra==chute):
+                print("Encontrei a letra {} na posição {}".format(letra, indice+1))
+            indice+=1
     print("Fim do jogo")
 if(__name__=="__main__"):
     jogar()
