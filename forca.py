0eb6b9647fde94741973cedb4c89b0243e498b64
gustavo-barrios2006 <gabarrios2006@gmail.com>
2024-07-03 15:00:18 -0300

    mostrar como está o que já foi encontrado até agora da palavra e mostrar com sublinhados os caracteres restantes.

--- forca.py
+++ forca.py
@@ -5,13 +5,16 @@ def jogar():
     palavra_secreta="banana"
     acertou=False
     enforcou=False
+    letras_acertadas=["_", "_", "_", "_", "_", "_"]
     while(not acertou and not enforcou):
         chute=input("Digite uma letra").strip()
         indice=0
         for letra in palavra_secreta:
             if(letra.upper()==chute.upper()):
+                letras_acertadas[indice]=letra
                 print("Encontrei a letra {} na posição {}".format(letra, indice+1))
             indice+=1
+        print(letras_acertadas)
     print("Fim do jogo")
 if(__name__=="__main__"):
     jogar()
