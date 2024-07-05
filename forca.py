16305f198d3a8325d1e1af3c1f52818554afd7e3
gustavo-barrios2006 <gabarrios2006@gmail.com>
2024-07-05 00:40:00 -0300

    informando ao usuário quantas tentativas ele ainda tem caso ele erre

--- forca.py
+++ forca.py
@@ -2,19 +2,28 @@ def jogar():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
-    palavra_secreta="banana"
+    palavra_secreta="BANANA"
     acertou=False
     enforcou=False
     letras_acertadas=["_", "_", "_", "_", "_", "_"]
+    erros=0
     while(not acertou and not enforcou):
-        chute=input("Digite uma letra").strip()
+        chute=input("Digite uma letra").strip().upper()
         indice=0
         for letra in palavra_secreta:
-            if(letra.upper()==chute.upper()):
+            if(letra==chute):
                 letras_acertadas[indice]=letra
                 print("Encontrei a letra {} na posição {}".format(letra, indice+1))
+            else:
+                erros+=1
             indice+=1
         print(letras_acertadas)
+        enforcou=erros==6
+        acertou="_" not in letras_acertadas
+    if(acertou):
+        print("Você ganhou!")
+    else:
+        print("Você foi enforcado!")
     print("Fim do jogo")
 if(__name__=="__main__"):
     jogar()
