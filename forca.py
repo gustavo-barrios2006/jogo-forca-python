4ccccf509caad67ca17eb90bbd6fddbe00506afe
Gustavo Barrios <gabarrios2006@gmail.com>
2024-07-05 09:30:02 -0300

    melhorando o loop para sair dele com break em vez das variáveis acertou e enforcou e fazendo com que o for percorra a lista de letras da palavra secreta apenas se encontrar ao menos uma ocorrência dessa letra.

--- forca.py
+++ forca.py
@@ -7,25 +7,28 @@ def jogar():
     enforcou=False
     letras_acertadas=["_", "_", "_", "_", "_", "_"]
     erros=0
-    while(not acertou and not enforcou):
+    while(True):
         chute=input("Digite uma letra").strip().upper()
+        if(chute not in palavra_secreta):
+            erros+=1
+            if(erros<6):
+                errosRestantes=6-erros
+                print("Você ainda tem {} {} antes de ser enforcado".format(errosRestantes, "erros permitidos" if errosRestantes>1 else "erro permitido"))
+                continue
+            else:
+                print("Você foi enforcado!")
+                break
         indice=0
         for letra in palavra_secreta:
-            if(letra==chute):
-                letras_acertadas[indice]=letra
-                print("Encontrei a letra {} na posição {}".format(letra, indice+1))
-            else:
-                erros+=1
-                if(erros<6):
-                    print("Você ainda tem {} erros permitidos antes de ser enforcado".format(6-erros))
             indice+=1
+            if(letra==chute):
+                letras_acertadas[indice-1]=letra
+
+                print("Encontrei a letra {} na posição {}".format(letra, indice))
         print(letras_acertadas)
-        enforcou=erros==6
-        acertou="_" not in letras_acertadas
-    if(acertou):
-        print("Você ganhou!")
-    else:
-        print("Você foi enforcado!")
+        if("_" not in letras_acertadas):
+            print("Você ganhou!")
+            break
     print("Fim do jogo")
 if(__name__=="__main__"):
     jogar()
